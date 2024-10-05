5e53629cd13a12abb94d30ea274bce8431e93e72
daggerspell <jmsblckmn@gmail.com>
2024-10-05 07:40:21 -0500

    Step 4 complete

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,32 @@
-from flask import jsonify
-from . import create_app
+from flask import request, jsonify
+from . import db, create_app
+from .models import Song
 
 app = create_app()
 
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify(message="Welcome to MusicFlowRadio!"), 201
+    return jsonify(message="Welcome to MusicFlowRadio!"), 200
 
 
 @app.route("/api/health", methods=["GET"])
 def health_check():
-    return jsonify(message="Health check successful!"), 201
+    return jsonify(message="Health check successful!"), 200
+
+
+@app.route("/songs", methods=["POST"])
+def add_song():
+    data = request.get_json()
+    new_song = Song(
+        title=data["title"],
+        artist=data["artist"],
+        duration=data["duration"],
+        file_path=data["file_path"],
+    )
+    db.session.add(new_song)
+    db.session.commit()
+    return jsonify(message="Song added successfully"), 201
 
 
 if __name__ == "__main__":
